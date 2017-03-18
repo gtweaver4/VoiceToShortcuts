d7c384f375f5a6a64e20bacd465dc530b5a3368c
gtweaver4 <gtweaver4@gmail.com>
2017-03-17 20:14:38 -0400

    added basic gui layout need to add functional methods to the gui

--- main.py
+++ main.py
@@ -16,17 +16,26 @@ current_profile.set("Profile: ")
 
 #this starts the program
 def start():
-	i=0
+	recognize.recognize()
 
 #this reads in the current profiles of the users
 #for the main window to be able to display them
 #returns an array of profiles
 def read_in_profiles():
-	i=0
+	return ["x"]
 
 #sets the current profile for the user
 def set_profile(name):
 	current_profile.set("Profile: " + name)
+
+#saves the profile the user creates
+def save():
+	i = 0
+
+def add_entry(voice, button, voiceArr,buttonArr):
+	voiceArr.append(voice.get())
+	buttonArr.append(button.get())
+	Label(create, text = ("Voice Shortcut: " + voice.get() + "Buttons Pressed: " + button.get())).pack()
 
 #this changes the frames
 def raise_frame(frame):
@@ -40,6 +49,9 @@ create = Frame(root)
 for frame in (home, edit, create):
     frame.grid(row=0, column=0, sticky='news')
 
+
+
+#######################
 #######################
 #HOME SCREEEN
 home_profile_label = Label(home, textvariable = current_profile)
@@ -49,9 +61,12 @@ Button(home, text='Create New Profile', command=lambda:raise_frame(create)).pack
 Button(home, text='Start Voice Shortcuts', command=lambda:start()).pack()
 Button(home, text='Cancel', command=lambda:sys.exit()).pack()
 
+
+
+#######################
 #######################
 #EDIT PROFILES
-test = ['one', 'two', 'three', 'four']
+test = read_in_profiles()
 Label(edit, text='Edit').pack()
 edit_profile_label = Label(edit, textvariable = current_profile)
 edit_profile_label.pack() #on seperate lines to allow the label to update
@@ -60,29 +75,43 @@ for x in range(0, len(test)):
 Button(edit, text='Home', command=lambda:raise_frame(home)).pack()
 Button(edit, text='Cancel', command=lambda:sys.exit()).pack()
 
+
+
+
+#######################
 #######################
 #CREATE PROFILES
 Label(create, text='Create Profile').pack()
 
 #createFrame1 is for the horizontal allignment of widgets
-createFrame1 = Frame(root)
+createFrame1 = Frame(create)
 Label(createFrame1, text = "Name of Profile: ").pack(side='left')
+
 profile_name = StringVar()
-profile_name_entry = Entry(create, textvariable = profile_name)
+profile_name_entry = Entry(createFrame1, textvariable = profile_name)
 profile_name_entry.pack(side = 'left')
 createFrame1.pack()
 
-Button(create, text='Cancel', command=lambda:sys.exit()).pack()
+createFrame2 = Frame(create)
+voice = StringVar()
+button = StringVar()
+voiceArr = []
+buttonArr = []
+Label(createFrame2, text = "Voice Shortcut: ").pack(side = 'left')
+Entry(createFrame2, textvariable = voice).pack(side='left')
+Label(createFrame2, text = "\tButton Combination: ").pack(side='left')
+Entry(createFrame2, textvariable = button).pack(side='left')
+createFrame2.pack()
+Button(create, text = "add", command = lambda:add_entry(voice, button, voiceArr,buttonArr)).pack()
 
-
+buttonFrame = Frame(create)
+Button(buttonFrame, text='Home', command=lambda:raise_frame(home)).pack(side='left')
+Button(buttonFrame, text='Save', command=lambda:save()).pack(side='left')
+Button(buttonFrame, text='Cancel', command=lambda:sys.exit()).pack(side='left')
+buttonFrame.pack()
 
 raise_frame(home)
 root.mainloop()
-
-
-
-
-
 
 #start the tk window
 while(True):
